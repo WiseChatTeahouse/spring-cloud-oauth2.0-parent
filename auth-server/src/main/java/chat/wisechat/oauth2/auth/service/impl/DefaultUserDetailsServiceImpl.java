2005ad2667b2f7dd905fbd07818334e0cc565908
siberia.hu <wisechatteahouse@gmail.com>
2025-10-15 20:12:23 +0800

    feat: OAuth2认证服务、客户端信息实现类、用户信息实现类

---
+++ auth-server/src/main/java/chat/wisechat/oauth2/auth/service/impl/DefaultUserDetailsServiceImpl.java
@@ -0,0 +1,12 @@
+package chat.wisechat.oauth2.auth.service.impl;
+
+import chat.wisechat.oauth2.auth.service.ChargeUserDetailsService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author Siberia.Hu
+ * @Date 2025/10/15 19:55
+ */
+@Component
+public class DefaultUserDetailsServiceImpl implements ChargeUserDetailsService {
+}
---
+++ auth-server/src/main/java/chat/wisechat/oauth2/auth/service/impl/SmsUserDetailsServiceImpl.java
@@ -0,0 +1,12 @@
+package chat.wisechat.oauth2.auth.service.impl;
+
+import chat.wisechat.oauth2.auth.service.ChargeUserDetailsService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author Siberia.Hu
+ * @Date 2025/10/15 19:58
+ */
+@Component
+public class SmsUserDetailsServiceImpl implements ChargeUserDetailsService {
+}
