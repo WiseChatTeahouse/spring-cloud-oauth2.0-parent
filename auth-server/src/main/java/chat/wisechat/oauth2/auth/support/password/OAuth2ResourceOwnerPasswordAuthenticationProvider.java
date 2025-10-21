bdb64ee626a4ddb1531192b51c315489c34cca82
siberia.hu <wisechatteahouse@gmail.com>
2025-10-21 16:08:29 +0800

    feat: 密码认证

--- auth-server/src/main/java/chat/wisechat/oauth2/auth/support/password/OAuth2ResourceOwnerPasswordAuthenticationProvider.java
+++ auth-server/src/main/java/chat/wisechat/oauth2/auth/support/password/OAuth2ResourceOwnerPasswordAuthenticationProvider.java
@@ -9,6 +9,6 @@ import chat.wisechat.oauth2.auth.support.base.OAuth2ResourceOwnerBaseAuthenticat
 public class OAuth2ResourceOwnerPasswordAuthenticationProvider extends OAuth2ResourceOwnerBaseAuthenticationProvider {
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return OAuth2ResourceOwnerPasswordAuthenticationToken.class.isAssignableFrom(authentication);
     }
 }
