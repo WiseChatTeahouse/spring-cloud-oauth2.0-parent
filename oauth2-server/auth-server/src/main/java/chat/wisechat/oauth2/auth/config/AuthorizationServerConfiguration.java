a3bef4556e0bf061af4868eacfbd2428f070d9e4
siberia.hu <wisechatteahouse@gmail.com>
2025-11-28 10:25:54 +0800

    feat: 先用jwt 后面看看能不能实现相关的接口重写一下

--- oauth2-server/auth-server/src/main/java/chat/wisechat/oauth2/auth/config/AuthorizationServerConfiguration.java
+++ oauth2-server/auth-server/src/main/java/chat/wisechat/oauth2/auth/config/AuthorizationServerConfiguration.java
@@ -2,7 +2,6 @@ package chat.wisechat.oauth2.auth.config;
 
 import chat.wisechat.oauth2.auth.handler.ProjectAuthenticationFailureHandler;
 import chat.wisechat.oauth2.auth.handler.ProjectAuthenticationSuccessHandler;
-import chat.wisechat.oauth2.auth.support.ProjectUser;
 import chat.wisechat.oauth2.auth.support.core.UserDetailsAuthenticationProvider;
 import chat.wisechat.oauth2.auth.support.password.PasswordAuthenticationConverter;
 import chat.wisechat.oauth2.auth.support.password.PasswordAuthenticationProvider;
@@ -12,22 +11,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.core.OAuth2Token;
-import org.springframework.security.oauth2.jwt.JwsHeader;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.token.DelegatingOAuth2TokenGenerator;
 import org.springframework.security.oauth2.server.authorization.token.JwtGenerator;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2RefreshTokenGenerator;
-import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.time.Instant;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * @Author Siberia.Hu
@@ -66,29 +59,8 @@ public class AuthorizationServerConfiguration {
         OAuth2AuthorizationService authorizationService = http.getSharedObject(OAuth2AuthorizationService.class);
         http.authenticationProvider(new UserDetailsAuthenticationProvider(userDetailsService));
         http.authenticationProvider(new PasswordAuthenticationProvider(authorizationService, authenticationManager,
-                new DelegatingOAuth2TokenGenerator(tokenGenerator(), new OAuth2RefreshTokenGenerator())));
+                new DelegatingOAuth2TokenGenerator(new JwtGenerator(http.getSharedObject(JwtEncoder.class)), new OAuth2RefreshTokenGenerator())));
         return build;
-    }
-
-    public OAuth2TokenGenerator<OAuth2Token> tokenGenerator() {
-        JwtGenerator jwtGenerator = new JwtGenerator(parameters -> {
-            JwtClaimsSet claims = parameters.getClaims();
-            JwsHeader jwsHeader = parameters.getJwsHeader();
-            Instant issuedAt = Instant.now();
-            Instant expiresAt = issuedAt.plusMillis(60L);
-            String token = UUID.randomUUID().toString().replace("-", "");
-            return new Jwt(token, issuedAt, expiresAt, jwsHeader.getHeaders(), claims.getClaims());
-        });
-        jwtGenerator.setJwtCustomizer(context -> {
-            JwtClaimsSet.Builder claims = context.getClaims();
-            claims.claim("license", "https://wisechat.chat");
-            String clientId = context.getAuthorizationGrant().getName();
-            claims.claim("clientId", clientId);
-            // 写入用户信息
-            ProjectUser pigUser = (ProjectUser) context.getPrincipal().getPrincipal();
-            claims.claim("user_info", pigUser);
-        });
-        return new DelegatingOAuth2TokenGenerator(jwtGenerator, new OAuth2RefreshTokenGenerator());
     }
 
 }
